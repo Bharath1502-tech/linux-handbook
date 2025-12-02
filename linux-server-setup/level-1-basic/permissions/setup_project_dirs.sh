a00dda6166e3fa2fb33e2363951a20286802ffd4
Bhararh M <bharathmorsu1502@gmail.com>
2025-12-02 14:02:13 +0530

    Delete linux-server-setup directory

--- linux-server-setup/level-1-basic/permissions/setup_project_dirs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo mkdir -p /srv/myapp/{logs,data}
-sudo chown -R root:devs /srv/myapp
-sudo chmod -R 2770 /srv/myapp
-
-echo "/srv/myapp ready with devs group permissions"
--- linux-server-setup/level-3-advanced/systemd/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec java -Xms512m -Xmx1g -jar /srv/myapp/myapp.jar
